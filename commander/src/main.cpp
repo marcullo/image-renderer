e58e6742763366e2fcbac64315def15b04c0983f
Lukasz Marcul <dev.marcul@gmail.com>
2020-11-13 21:30:13 +0100

    commander: Process commands from standard input
    When you see a command prompt, type the command with arguments and
    hit ENTER. Data will be proxied to the child program renderer which
    will take care of it.

--- commander/src/main.cpp
+++ commander/src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+#include "Commander.hpp"
 #include "execution.hpp"
 
 #include "log_settings.hpp"
@@ -9,11 +10,51 @@
 
 using namespace commander;
 
-int main(void)
+namespace {
+
+int process_from_stdin()
 {
+	std::string command{""};
+	Commander commander;
+
+	while (true) {
+		/* Type command. */
+		std::cout << "> ";
+		if (!std::getline(std::cin, command)) {
+			break;
+		}
+
+		/* process command. */
+		try {
+			commander += command;
+			commander.process();
+		}
+		catch (std::exception& e) {
+			ERR("%s", e.what());
+		}
+	}
+
+	return EXIT_SUCCESS;
+}
+
+} // namespace
+
+int main(int argc, char* argv[])
+{
+	(void) argv;
+
+	if (argc > 2) {
+		std::cout << "Usage: ./commander [commands_filename]\n";
+		return EXIT_FAILURE;
+	}
+
 	if (execute_renderer() != SlaveState::RUN) {
 		std::cout << "Failed to fork renderer! Make sure you're located in the same folder as me.";
 		return EXIT_FAILURE;
+	}
+
+	if (argc == 1) {
+		return process_from_stdin();
 	}
 
 	return EXIT_SUCCESS;
