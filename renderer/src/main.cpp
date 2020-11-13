ab9001847d222d715885ce3b054ef1eb1dd86a79
Lukasz Marcul <dev.marcul@gmail.com>
2020-11-13 20:32:49 +0100

    renderer: Implement main behavior
    Loop over commands received from the master and react on them with
    rendering or error handling. Send empty response as an ack message
    for valid commands.

--- renderer/src/main.cpp
+++ renderer/src/main.cpp
@@ -1,13 +1,41 @@
-#include <iostream>
+#include "Channel.hpp"
+#include "exceptions.hpp"
+#include "Renderer.hpp"
 
 #include "log_settings.hpp"
 #define LOG_LEVEL LOG_LEVEL__RENDERER
 #define LOG_LEVEL_MODULE_NAME "renderer"
 #include "logging.hpp"
 
+using namespace renderer;
+namespace vs = sdk::validation;
+
 int main(void)
 {
-	INF("Hello in " BUILD_MODE_STR "!");
+	sdk::channel::ChannelMOSI mosi{false};
+	sdk::channel::ChannelMISO miso{false};
+	Renderer::rand_t dev;
+	Renderer rr{dev};
+
+	while (true) {
+		try {
+			std::string request = mosi.receive();
+			rr.process(request);
+
+			std::string ok_response{""};
+			miso.send(ok_response);
+		}
+		catch (vs::VerificationException& e) {
+			ERR("%s", e.what());
+			std::string response{e.what()};
+			miso.send(response);
+		}
+		catch (std::exception& e) {
+			ERR("%s", e.what());
+			std::string response{e.what()};
+			miso.send(response);
+		}
+	}
 
 	return EXIT_SUCCESS;
 }
