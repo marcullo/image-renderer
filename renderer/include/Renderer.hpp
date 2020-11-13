63e445d64d80b415e6e1576958f1ba9f025a3c28
Lukasz Marcul <dev.marcul@gmail.com>
2020-11-13 20:00:26 +0100

    renderer: Modify the image according to commands
    The background is white. The foreground color is randomized. If you
    extend the image, new part gets the color of background.

--- renderer/include/Renderer.hpp
+++ renderer/include/Renderer.hpp
@@ -3,15 +3,17 @@
 
 #include "CommandParser.hpp"
 #include "Dicer.hpp"
+#include "Image.hpp"
 
 namespace renderer {
 
 class Renderer {
 public:
+	using bg_t = sdk::graphics::Image::paint_t;
 	using rand_t = std::random_device;
 
-	/** Setup render. */
-	Renderer();
+	/** Setup render with random device for foreground color. */
+	Renderer(rand_t& dev);
 
 	/** Do not setup renderer from other instances. */
 	Renderer(const Renderer&) = delete;
@@ -21,8 +23,11 @@ public:
 
 protected:
 	using parser_t = sdk::shell::CommandParser;
+	using dicer_t = sdk::graphics::Image::dicer_t;
+	using image_t = sdk::graphics::Image;
 	using command_t = sdk::shell::Command;
 
+	image_t image; /** Data image object. */
 	parser_t parser; /** Command parser. */
 };
 
--- renderer/src/Renderer.cpp
+++ renderer/src/Renderer.cpp
@@ -8,7 +8,8 @@
 
 namespace renderer {
 
-Renderer::Renderer() :
+Renderer::Renderer(rand_t& dev) :
+	image(dicer_t{dev}, bg_t{255, 255, 255}),
 	parser(std::move(sdk::shell::CommandParser::commands_t{commands()}))
 {}
 
@@ -16,6 +17,41 @@ void Renderer::process(std::string& command)
 {
 	auto cmd = parser.parse(command);
 	renderer::log_command(cmd);
+
+	switch (CommandType(cmd->type)) {
+	case CommandType::SET_WIDTH: {
+		const auto& c = std::dynamic_pointer_cast<CommandSetWidth, command_t>(cmd);
+		image.set_width(c->w);
+		break;
+	}
+
+	case CommandType::SET_HEIGHT: {
+		const auto& c = std::dynamic_pointer_cast<CommandSetHeight, command_t>(cmd);
+		image.set_height(c->h);
+		break;
+	}
+
+	case CommandType::DRAW_RECTANGLE: {
+		const auto& c = std::dynamic_pointer_cast<CommandDrawRectangle, command_t>(cmd);
+		image.draw_rectangle(c->x, c->y, c->w, c->h);
+		break;
+	}
+
+	case CommandType::DRAW_TRIANGLE: {
+		const auto& c = std::dynamic_pointer_cast<CommandDrawTriangle, command_t>(cmd);
+		image.draw_triangle(c->x1, c->y1, c->x2, c->y2, c->x3, c->y3);
+		break;
+	}
+
+	case CommandType::RENDER: {
+		const auto& c = std::dynamic_pointer_cast<CommandRender, command_t>(cmd);
+		image.render(c->filename);
+		break;
+	}
+
+	default:
+		break;
+	}
 }
 
 } // namespace renderer
